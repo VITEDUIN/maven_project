959d021ae18013375b8867d7f09464e9a6d27425
Bushra Shaikh <bushra.sk@siesgst.ac.in>
2025-03-06 16:49:24 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -5,15 +5,29 @@ import org.junit.Before;
 import org.junit.Test; 
 import org.openqa.selenium.WebDriver; 
 import org.openqa.selenium.chrome.ChromeDriver; 
- 
+ import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v133.browser.Browser;
 public class AppTest { 
-    private WebDriver driver; 
+    //private WebDriver driver; 
  
     @Before 
     public void setUp() { 
         // Set the path for the ChromeDriver executable 
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe"); 
-        driver = new ChromeDriver(); 
+       // driver = new ChromeDriver(); 
+     ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-debugging-port=9222"); // Enable DevTools via remote debugging
+
+        // Initialize the WebDriver
+        WebDriver driver = new ChromeDriver(options);
+
+        // Get the DevTools instance
+        DevTools devTools = ((ChromeDriver) driver).getDevTools();
+
+        // Create a DevTools session and enable the necessary protocols
+        devTools.createSession();
+        devTools.send(Browser.enable());
+
     } 
  
     @Test 
