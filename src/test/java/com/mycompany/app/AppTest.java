14447bd8fcbc32787831e93613c14c61972b16a4
Bushra Shaikh <bushra.sk@siesgst.ac.in>
2025-03-06 16:54:55 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver; 
  import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v133.browser.Browser;
+import org.openqa.selenium.chrome.ChromeOptions;
 public class AppTest { 
     //private WebDriver driver; 
  
